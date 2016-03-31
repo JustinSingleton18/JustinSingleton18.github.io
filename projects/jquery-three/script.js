0bfed877d90254b49886325f6c0010759e9922f5
justinsingleton181 <justinjsingleton88+1@gmail.com>
2016-03-31 20:09:55 +0000

    my work

--- projects/jquery-three/script.js
+++ projects/jquery-three/script.js
@@ -59,47 +59,55 @@ $(function () {
   
   // ALL YOUR CODE BELOW HERE //
   
+  // Merge the arrays of objects into one array of objects.
+  var allGuardians = _.flatten([oldGuardians, newGuardians]);
+  console.log(allGuardians);
   
-  
-// Merge the arrays of objects into one array of objects.
-var allGuardians = _.flatten([oldGuardians, newGuardians]);
-console.log(allGuardians);
-
-// Use map to make an array of all of the names of your heros.
-var heroNames =_.map(allGuardians, function(g) {
-  return g.name;
-});
-console.log(heroNames);
-// Now how about modifying your last step to pull out just first names!
-var heroFirstNames = _.map(heroNames, function(n) {
-  return n.split(' ')[0];
-});
-console.log(heroFirstNames);
-// Filter out `Groot`.
-var allButGroot = _.filter(heroFirstNames, function(n) {
-  return n !== 'Groot';
-});  
-console.log(allButGroot);
-
-// Use your new found knowledge of making tables with jQuery to create a list.
-function createHeroesList (heros) {
-  // create an unordered list(<ul>) of heroes from the provided array.
-  var $ul = $('<ul>').attr('id', 'heros');
-    
-  // give each <li> in the <ul> the heros's name and notes
-  var $lis = _.map(heros, function(h) {
-    var $li = $('<li>');
-    var $namePar = $('<p>').text("Name:" + h.name);
-    var $notesPar = $('<p>').text("Notes:" + h.notes);
-    $li.append([$namePar, $notesPar]);
-    return $li;
+  // Use map to make an array of all of the names of your heros.
+  var heroNames = _.map(allGuardians, function(g){
+    return g.name;
   });
-  $ul.append($lis);
-  // append the <ul> to the DOM
-  $('body').append($ul);
-}
-createHeroesList(allGuardians);  
+  console.log(heroNames);
   
+  // Now how about modifying your last step to pull out just first names!
+  
+  var heroFirstNames = _.map(heroNames, function(n) {
+    return n.split(' ')[0];      
+  });
+  console.log(heroFirstNames);
+  
+  
+  // Filter out `Groot`.
+  
+  var allButGroot = _.filter(heroFirstNames, function (n){
+    return n !== 'Groot';
+  });
+  console.log(allButGroot);
+  
+  
+  
+  // Use your new found knowledge of making tables with jQuery to create a table showing the heros and their notes
+  //creating a list instead
+  function createHeroesList (heroes) {
+    //create an unordered list of heroes from the provided array
+    var $ul = $('<ul>').attr('id', 'heroes');
+    //give each <li> in the <ul> the hero's name and notes
+    var $lis = _.map(heroes, function(h) {
+      var $li = $('<li>');
+      
+      var $namePar = $('<p>').text('Name: ' + h.name);
+      var $notesPar = $('<p>').text('Notes: ' + h.notes);
+      
+      $li.append([$namePar, $notesPar]);
+      return $li;
+    });
+    //append <li>s to the <ul>
+    $ul.append($lis);
+    
+    //append the <ul> to the DOM
+    $('body').append($ul);
+  }
+  createHeroesList(allGuardians);
   
   // ALL YOUR CODE ABOVE HERE //
 });
--- projects/jquery-two/script.js
+++ projects/jquery-two/script.js
@@ -1,49 +1,77 @@
-// $(function(){
-//   var avengers = ["Iron Man", "Thor", "Hulk", "Ant Man", "Wasp"];
+$(function(){
+  var avengers = ["Iron Man", "Thor", "Hulk", "Ant Man", "Wasp"];
 
-// // Write and call a function that expects an array as an argument. 
-// // Within this function, create a <ul> with the ID avengers and append it to the DOM. 
-
-// // Next, still within this function, write a loop that creates an <li> and appends it to the ul for each item in the array. 
-// // For each item, add a class corresponding to the item's content.
-// // For example, the first item in the array should have an element that looks like <li class="iron-man">Iron Man</li>.
-
-// // Hint
-// // .split(), .join(), .toLowerCase() and .replace() are some useful functions you might investigate.
-
-// function createAvengers(avengers) {
-  
-//   var $avengers = $('<ul>');
-//   //.attr('id', 'avengers');
-  
-//   var listItems = avengers.map(CreateAvenger);
-      
-//     var kebab = avenger.toLocaleLowerCase().replace(" ", "-");
-//     $li.addClass(kebab);
+  // Code goes here!
+  function createAvenger(avenger){
+    var $li = $('<li>')
+      .text(avenger);
     
-//     return $li;
+    var kebab = avenger.toLowerCase().replace(' ', '-');
+    $li.addClass(kebab);
     
-
+    return $li;
+  }
   
-//   $avengers.append(listItems);
   
-//   $('body').append($avengers);
-
-// createAvengers(avengers);
-
-// //Write and call a new function that removes the <li>s for Ant Man and Wasp, and adds an <li> for Captain America.
-// function avengersReassemble(){
-//   var $avengers = $('#avengers');
+ function createAvengers (avengers){
+  // Write and call a function that expects an array as an argument. 
+  // Within this function, append a <ul> to the DOM with the ID avengers.
   
-// $('.ant-man, .wasp', $avengers).remove();
-// var $captAmerica = $('<li>')
-// .addClass('captain-america')
- 
-// $avengers.append($captAmerica);
-// }
-// avengersReassemble();
+  //when adding to the DOM we usually mean the body
+  
+  var $avengers = $('<ul>')
+    .attr('id','avengers');
+    
+  var listItems = avengers.map(createAvenger);
+  
+  $avengers.append(listItems);
+    
+  $('body').append($avengers);
+  
+  listItems.forEach(function($li, i){
+    $li 
+      .hide()
+      .delay(i*200)
+      .fadeIn();
+  });
+  
 
-// $('.avengers-assemble').on('click', function (event){
-//   createAvengers(avengers);
-// })
-// });
+  // Next, still within this function, write a loop that creates an <li> and 
+  // appends it to the ul for each item in the array. For each item, 
+  // add a class corresponding to the item's content. For example, 
+  // the first item in the array should have an element that looks like <li class="iron-man">Iron Man</li>.
+  
+  }
+  // createAvengers(avengers);
+  
+  
+  // Write and call a new function that removes the <li>s for Ant Man and Wasp, and adds an <li> for Captain America.
+  function avengersReassemble(){
+    var $avengers = $('#avengers');
+    
+    $('.ant-man, .wasp', $avengers).remove();
+    var $captAmerica = createAvenger('Captain America');
+    
+    // $('<li>')
+    //   .addClass('captain-america')
+    //   .text('Captain America');
+    $avengers.append($captAmerica);
+    $avengers.children().each(function(i, li){
+    $(li) 
+      .hide()
+      .delay(i*200)
+      .fadeIn();
+  });
+  }
+  // avengersReassemble();
+
+  $('.avengers-assemble').on('click', function (event){
+    createAvengers(avengers);
+  });
+  
+  $('.avengers-reassemble').on('click', function (event){
+    avengersReassemble();
+  });
+  
+  
+});
